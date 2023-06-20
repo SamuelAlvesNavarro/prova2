b8a42fe370a5919371b397d13f6816f74520631e
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-06-20 09:49:02 -0300

    a

--- forms/index.php
+++ forms/index.php
@@ -5,19 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
 <body>
 
-    <h1>Menu Principal</h1>
+    <h1 class="text-center">Menu Principal</h1>
 
-    <nav class="navbar navbar-dark bg-dark">
-    <div class="collapse navbar-collapse">
-        <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"><a class="nav-link"  href="cadastro_fluxo.php">Cadastro Fluxo de Caixa</a></li>
-                    <li class="nav-item active"><a class="nav-link"  href="listar_fluxo.php">Listagem de Fluxo de Caixa</a></li>
-                    <li class="nav-item active"><a class="nav-link"  href="consulta_fluxo_caixa.html">Consulta do Saldo</a></li>
-        </ul>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active"><a class="nav-link"  href="cadastro_fluxo.php">Cadastro Fluxo de Caixa</a></li>
+                <li class="nav-item active"><a class="nav-link"  href="listar_fluxo.php">Listagem de Fluxo de Caixa</a></li>
+                <li class="nav-item active"><a class="nav-link"  href="consulta_fluxo_caixa.html">Consulta do Saldo</a></li>
+            </ul>
+        </div>
     </nav>
 
 </body>
